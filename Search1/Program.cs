bbff0ede5b2794425d889568ce23249d0a8eae94
Tony Collins <tjc@eltham-college.org.uk>
2020-12-10 10:47:15 +0000

    Add project files.

---
+++ Search1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Search1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string[] students = { "jkashdfkhdfhfdas", "ukgh", "hgkjk", "Seb", "Tom" };
+
+            findPerson(students);
+        }
+
+        static void findPerson(string[] v)
+        {
+            string s;
+            Console.WriteLine("Who do you want to find?");
+            s = Console.ReadLine();
+            bool found = false; 
+
+            for (int i = 0; i<v.Length; i++)
+            {
+                if (v[i] == s)
+                {
+                    Console.WriteLine("Found them at pos {0}", i);
+                    found = true;
+                }
+            }
+
+        }
+    }
+}
